59a8c58b33c4e88b2755978f845405cfaaf2c10d
dean-alphafx <65293263+dean-alphafx@users.noreply.github.com>
2021-02-26 22:27:15 +0000

    Update install_worker.sh

--- cluster-setup/latest/install_worker.sh
+++ cluster-setup/latest/install_worker.sh
@@ -50,6 +50,12 @@ sysctl --system >/dev/null 2>&1
 
 systemctl enable kubelet && systemctl start kubelet
 
+curl -s https://falco.org/repo/falcosecurity-3672BA8F.asc | apt-key add -
+echo "deb https://dl.bintray.com/falcosecurity/deb stable main" | tee -a /etc/apt/sources.list.d/falcosecurity.list
+apt-get update -y
+apt-get -y install linux-headers-$(uname -r)
+apt-get install -y falco
+systemctl start falco
 
 ### init k8s
 kubeadm reset -f
